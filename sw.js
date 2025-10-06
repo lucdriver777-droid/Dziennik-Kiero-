f9e57aa688d85d039fba12ac9d30c7fa76b9cea4
lucdriver777-droid <luc.driver777@gmail.com>
2025-10-06 23:10:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,20 @@
-const CACHE_NAME = 'dziennik-kiero-v1';
-const OFFLINE_URL = './offline.html';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  OFFLINE_URL
-];
-
-// Instalacja
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("dziennik-kierowcy-v1").then(cache => {
+      return cache.addAll([
+        "/Dziennik-Kiero-/",
+        "/Dziennik-Kiero-/index.html",
+        "/Dziennik-Kiero-/manifest.json",
+        "/Dziennik-Kiero-/icon-192.png",
+        "/Dziennik-Kiero-/icon-512.png"
+      ]);
+    })
   );
-  self.skipWaiting();
+  console.log("Service Worker zainstalowany ✅");
 });
 
-// Aktywacja
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
-  self.clients.claim();
-});
-
-// Fetch
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(resp => resp || fetch(event.request))
-    );
-  }
 });
